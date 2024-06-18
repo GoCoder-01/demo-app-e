ad05cb74d54a511fa6eb1d186054098dc6002081
OM SANTOSH GAWANDE <gawandeom98@gmail.com>
2024-06-18 15:34:28 +0530

    Demo Ecommerce project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-orange-600 font-bold tracking-wide'>Welcome</h1>
+      <button className='bg-green-600 py-1 px-3 text-white rounded-sm font-normal tracking-wide text-xl shadow-sm '>log in</button>
+    </>
+  )
+}
+
+export default App;
