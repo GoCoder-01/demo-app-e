e3a59bd9424a2a0abeffb8e64b5b1dbff5409be4
OM SANTOSH GAWANDE <gawandeom98@gmail.com>
2024-06-19 17:09:34 +0530

    Created Login form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import React from 'react';
+import Login from './login/login';
+import { useState } from 'react';
 import './App.css'
 
 function App() {
+  const [isActive, setIsActive] = useState(false);
+  if(!isActive){
+    return (
+      <>      
+        <h1 className='text-orange-600 font-bold tracking-wide'>Welcome</h1>
+        <button onClick={() => setIsActive(!isActive)} className='bg-green-600 py-1 px-3 text-white rounded-sm font-normal tracking-wide text-xl shadow-sm'>log in</button>
+      </>
+    );
+  }
   return (
-    <>
-      <h1 className='text-orange-600 font-bold tracking-wide'>Welcome</h1>
-      <button className='bg-green-600 py-1 px-3 text-white rounded-sm font-normal tracking-wide text-xl shadow-sm '>log in</button>
-    </>
+    <div className='flex justify-center mt-24'>
+      <Login userName="OM GAWANDE"/>
+    </div>
+    
   )
 }
 
--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,7 +1,17 @@
-function Login(){
+function Login({userName}){
     return(
         <>
-            <h1>Log in form</h1>
+            <div className="flex flex-col w-fit p-4  bg-white rounded-lg">
+                <form className="flex flex-col align-middle">
+                    <h1 className="text-blue-800 font-extrabold tracking-wide text-lg text-center">Log in</h1>
+                    <input className="py-1 pl-1 outline-none my-2 border-2 rounded-lg" type="text" placeholder="username"/>
+                    <input className="py-1 pl-1 outline-none my-2 border-2 rounded-lg" type="password" placeholder="password" />
+                    <button onClick={(e) => {
+                        e.preventDefault();
+                        setIsActive(!isActive);
+                    }} className="p-2 mt-4 bg-blue-950 text-white rounded-lg font-medium" type="submit">Log in</button>
+                </form>
+            </div>
         </>
     )
 }
